dc6b50dde8f9b3c0b41588c6ac0aa343725b14e2
buithang1401 <87681317+buithang1401@users.noreply.github.com>
2021-08-12 21:13:26 +0700

    enter

---
+++ djangoProject/mysite/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm, widgets
+from .models import Title
+from django import forms
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Title
+        fields = ['title', 'author', 'body_text','date']
+
+        widgets ={
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'author': forms.Select(attrs={'class':'form-control'}),
+            'bodytext': forms.Textarea(attrs={'class':'form-control'}),
+            'date': forms.DateInput(attrs={'class':'form-control'}),
+
+            
+        }
+
+      
+

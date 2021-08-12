c57fa19b54b40b6961693f1a05457ee0ef930b90
buithang1401 <87681317+buithang1401@users.noreply.github.com>
2021-08-12 21:08:52 +0700

    enter

---
+++ djangoProject/mysite/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Title
+
+
+# Register your models here.
+admin.site.register(Title)
+#admin.site.register(User)
+#admin.site.register(SuperUser)
+#admin.site.register(UserManager)
+
+
